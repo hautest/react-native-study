5edaf24235ffeaef6a4ed4d102a4b107439166e6
LeeSeungHun <dos07008@naver.com>
2024-08-20 13:18:20 +0900

    Button 추가해보기

--- app/my.tsx
+++ app/my.tsx
@@ -1,9 +1,25 @@
-import { Text, View } from "react-native";
+import { Button, Text, View, Pressable } from "react-native";
 
 export default function MyPage() {
   return (
-    <View style={{ marginTop: 100, backgroundColor: "red" }}>
+    <View style={{ marginTop: 100 }}>
       <Text>my page</Text>
+      <Button title="test" color="red" />
+      <Pressable
+        style={{
+          backgroundColor: "white",
+          borderColor: "gray",
+          borderRadius: 8,
+          borderWidth: 2,
+          height: 40,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+        onPress={() => alert("tes")}
+      >
+        <Text style={{ color: "green" }}>123</Text>
+      </Pressable>
     </View>
   );
 }
